8d3f5077feeec014ef13664f62fb524ac0ca3574
Kemal <kemalburak.talas@idb.jsb>
2024-08-12 15:00:25 +0300

    initial commit

---
+++ proj/CMakeFiles/3.24.3/CompilerIdC/CompilerIdC.build/Debug-iphoneos/CompilerIdC.build/Objects-normal/arm64/CMakeCCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/kemal/Desktop/PistiReal/build/ios/proj/CMakeFiles/3.24.3/CompilerIdC/CMakeCCompilerId.c
---
+++ proj/CMakeFiles/3.24.3/CompilerIdCXX/CompilerIdCXX.build/Debug-iphoneos/CompilerIdCXX.build/Objects-normal/arm64/CMakeCXXCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/kemal/Desktop/PistiReal/build/ios/proj/CMakeFiles/3.24.3/CompilerIdCXX/CMakeCXXCompilerId.cpp
